4981c73d087c431e9426b56d01b2e286265af488
Sabin Saju <138128793+sabinsaju32@users.noreply.github.com>
2025-10-22 15:41:22 +0530

    implemented response filter

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import cookieParser from 'cookie-parser';
 import { LoggingInterceptor } from './interceptors/logging-interceptors';
+import { ResponseInterceptor } from './interceptors/response-interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser()); 
   if(process.env.NODE_ENV!=='production')
   {
-    app.useGlobalInterceptors(new LoggingInterceptor());
+    app.useGlobalInterceptors(new LoggingInterceptor(),new ResponseInterceptor());
   }
   await app.listen(process.env.PORT ?? 3000);
 }
